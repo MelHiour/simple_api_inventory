925a94b7053bb5e9de3362c58f7c40806416c1d1
root <root@everest.prod.mnlth.labnfun.ru>
2021-07-31 23:21:08 +0100

    Starting writing tests

---
+++ test/test_helpers.py
@@ -0,0 +1,11 @@
+import pytest
+from helpers import Inventory
+
+
+def test_inventory_init_from_file(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "test.json"
+    p.write_text('{"test": {"name": "test"}}')
+    inventory = Inventory(p)
+    assert inventory.test.to_dict() == {'name':'test'}
