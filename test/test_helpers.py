7287bc291d10731dd54ef72f419a89f9aaab4443
root <root@everest.prod.mnlth.labnfun.ru>
2021-08-01 10:05:07 +0100

    Adding tests for helpers.py

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from helpers import Inventory
+from helpers import Equipment, Inventory
 
 
 def test_inventory_init_from_file(tmp_path):
@@ -9,4 +9,68 @@ def test_inventory_init_from_file(tmp_path):
     p = d / "test.json"
     p.write_text('{"test": {"name": "test"}}')
     inventory = Inventory(p)
-    assert inventory.test.to_dict() == {'name':'test'}
+    assert inventory.test.to_dict() == {'name': 'test'}
+
+
+def test_inventory_init_from_empty_file(tmp_path):
+    inventory = Inventory('sub/test.json')
+    assert inventory.__dict__ == {'_db_file': 'sub/test.json'}
+
+
+def test_inventory_dumps_to_file(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "test.json"
+    inventory = Inventory(p)
+    inventory.test = Equipment({'name': 'test'})
+    inventory.sync()
+    assert p.read_text() == '{"test": {"name": "test"}}'
+
+
+def test_inventory_add_equipment(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "test.json"
+    inventory = Inventory(p)
+    inventory.add_equipment({'name': 'test'})
+    assert inventory.test.to_dict() == {'name': 'test'}
+
+
+def test_inventory_get_equipment(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "test.json"
+    inventory = Inventory(p)
+    inventory.add_equipment({'name': 'test1'})
+    inventory.add_equipment({'name': 'test2'})
+    assert inventory.get_equipment() == ['test1', 'test2']
+
+
+def test_inventory_get_equipment_attr(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "test.json"
+    inventory = Inventory(p)
+    inventory.add_equipment({'name': 'test', 'get_me': 'got_me'})
+    assert inventory.get_equipment_attr('test', 'get_me') == 'got_me'
+
+
+def test_inventtory_update_equipment_attr(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "test.json"
+    inventory = Inventory(p)
+    inventory.add_equipment({'name': 'test', 'change_me': 'before'})
+    inventory.update_equipment_attr('test', {'change_me': 'after'})
+    assert inventory.test.change_me == 'after'
+
+
+def test_inventory_del_equipment(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "test.json"
+    inventory = Inventory(p)
+    inventory.add_equipment({'name': 'test1'})
+    inventory.add_equipment({'name': 'test2'})
+    inventory.del_equipment('test1')
+    assert inventory.get_equipment() == ['test2']
